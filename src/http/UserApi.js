2ac05fce54f2e3caa9c80d935cdcb82d300fbb55
MalcevStepan <stiepan-maltsiev@mail.ru>
2023-09-08 12:13:43 +0300

    remove alert

--- src/http/UserApi.js
+++ src/http/UserApi.js
@@ -3,7 +3,6 @@ import { $authHost, $host } from "./index";
 export const loginFunc = async (login, password) => {
     const { data } = await $host.post('/login', { login: login, password: password });
     localStorage.setItem('token', data.token);
-    alert("token " + data.token);
     return data;
 }
 
